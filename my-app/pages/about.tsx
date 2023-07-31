3a71a939299688d597f0cb3b5e86e7ed28480fd8
Nate Levinson <natelevinson10@gmail.com>
2023-07-30 20:27:57 -0400

    Minor content changes

--- my-app/pages/about.tsx
+++ my-app/pages/about.tsx
@@ -22,7 +22,7 @@ export default function About() {
             <div className={styles.about}>
               <div className={styles.aboutText}>
                 <p>
-                  Hello! I am a junior at <strong>Wesleyan University</strong> pursuing a Bachelor of Arts in  Computer Science. I'm 19 years old and am from <strong>Philadelphia, PA</strong> (go birds🦅). I have a deep passion for learning and problem solving, and love collaborating with others. In terms of technical skills, I am proficient in <strong>Python</strong>  and <strong>C</strong>, and have experience with HTML/CSS, SMLNJ, and JavaScript. I'm looking to improve, expand, and apply my skillset to solve problems and learn through projects and internships.
+                  Hello! I am a junior at <strong>Wesleyan University</strong> pursuing a Bachelor of Arts in  Computer Science. I'm 19 years old and am from <strong>Philadelphia, PA</strong> (go birds🦅). I have a deep passion for learning and problem-solving and love collaborating with others. In terms of technical skills, I am proficient in <strong>Python</strong>  and <strong>C</strong>, and have experience with HTML/CSS, SMLNJ, and JavaScript. I'm looking to improve, expand, and apply my skillset to solve problems and learn through projects and internships.
                 </p>
                 <p>
                   In my free time, I enjoy playing soccer, tennis, and chess, running on local trails, watching shows (currently Criminal Minds and Suits), and challenging my friends to a game of Mario Kart. Outside of Computer Science, I love learning about history - especially about the Dark Ages🏰 and ancient Greek society / mythology🏛️. Music has become a major part of my life as I spend a few hours every day listening to my favorite artists. Currently, I'm into Red Hot Chilli Peppers🌶️, The Smashing Pumpkins🎃, and Daft Punk🤖. I have a passion for entrepreneurship and find joy in exploring innovative ideas and creating new ventures that have the potential to make a positive impact.
@@ -40,8 +40,8 @@ export default function About() {
                 <ul>
                   <li>• I recently picked up <strong>electric guitar</strong> 🎸 (sincere apologies to my family members who have no other option but to endure the sound of me practicing in my room)</li>
                   <li>• I am very interested in  <strong >geography</strong> 🌎. I love to play geoguessr, memorize country/state/territory flags and cities, and learn obscure facts about foreign places. Did you know the country of Tuvalu does not accept credit or debit cards anywhere?</li>
-                  <li>• I have never traveled outside of America, and would love to visit<strong > Italy🍕.</strong> The culture (especially the food) is something I have always wanted to exerience</li>
-                  <li>• My favorite animal is a <strong >penguin</strong>🐧, and in a truly shocking turn of events, my favorite movie has and always will be The Penguins of Madacascar.</li>
+                  <li>• I have never traveled outside of America and would love to visit<strong > Italy🍕.</strong> The culture (especially the food) is something I have always wanted to experience</li>
+                  <li>• My favorite animal is a <strong >penguin</strong>🐧, and in a truly shocking turn of events, my favorite movie has been and always will be The Penguins of Madagascar.</li>
                 </ul>
               </div>
             </div>
