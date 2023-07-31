237ad8197d23ca38c47c347d72012225eb096603
Nate Levinson <natelevinson10@gmail.com>
2023-07-30 20:39:13 -0400

    add insights

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
+  <Analytics />
   return <Component {...pageProps} />
 }
